bc30aadf5a4ad6670087266a6202c1596c6fcf3d
Mussab ElDash <MussabElDash@users.noreply.github.com>
2018-03-21 16:02:09 -0500

    Create accessories.js

---
+++ MetroPCS Helper/accessories.js
@@ -0,0 +1,54 @@
+var accessories = {
+			'keys' : ['case', 'hp', 'tgs', 'popSocket', 'metro8gb', 'other8gb', '3600', 'smallAtalax', 'mediumAtalax', 'bigAtalax'],
+			'case' : {
+				'cost' : 25,
+				'limit' : 15
+			},
+			'hp' : {
+				'cost' : 15,
+				'limit' : 10
+			},
+			'tgs' : {
+				'cost' : 15,
+				'limit' : 10
+			},
+			'popSocket' : {
+				'cost' : 15,
+				'limit' : 10
+			},
+			'metro8gb' : {
+				'cost' : 20,
+				'limit' : 15
+			},
+			'other8gb' : {
+				'cost' : 25,
+				'limit' : 20
+			},
+			'3600' : {
+				'cost' : 30,
+				'limit' : 20
+			},
+			'smallAtalax' : {
+				'cost' : 49,
+				'limit' : 40
+			},
+			'mediumAtalax' : {
+				'cost' : 70,
+				'limit' : 60
+			},
+			'bigAtalax' : {
+				'cost' : 120,
+				'limit' : 100
+			},
+			most: function(cost){
+				var max = 0;
+				var name;
+				this.keys.forEach(function(elem){
+					if(accessories[elem].limit > max && accessories[elem].limit < cost){
+						max = accessories[elem].limit;
+						name = elem;
+					}
+				});
+				return name;
+			},
+		}
